6af046b08c53feedad9ab7309b85464d4f44e5c7
rafael madrigal <rfgmadrigal@gmail.com>
2024-04-03 20:26:29 -0400

    revised app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ GLOBAL_LLM_MODELS = {
 LLM_DESCRIPTIONS = {
     'Mistral-7B-Instruct-v0.2' :  """Mistral is an instruct fine-tuned version of Mistral-7B-v0.2 available on HuggingFace (Open Source)""",
     'OpenAI (gpt-3.5-turbo)' : """GPT-3.5 is a model developed by OpenAI""",
-    'OpenAI (gpt-4)' : """GPT-3.5 is a model developed by OpenAI"""
+    'OpenAI (gpt-4)' : """GPT-4 is a model developed by OpenAI"""
 }
 
 LLM_DISCLAIMERS = {
@@ -74,7 +74,7 @@ def show_chat_messages():
         with st.chat_message(message['role']):
             st.markdown(message['content'])
 
-def load_files():
+def load_files_and_get_chain():
 
     splits = []
     for file in st.session_state['pdfs']:
@@ -84,88 +84,99 @@ def load_files():
         
             splits_i = load_and_split_documents(tmp_file_path) 
             splits += splits_i
-    if 'vector_db' not in st.session_state.keys():
-        st.session_state['vector_db'] = []
-    else: 
-        st.session_state['vector_db'] = store_documents(splits)
-    # print('VectorDB loaded')
-    
-    
 
+    st.session_state['vector_db'] = store_documents(splits)
 
+    st.session_state['chain'] = LCELBaseChatbot(llm=st.session_state['llm'], 
+                                    vectordb=st.session_state['vector_db'], 
+                                    message_history=st.session_state['messages'])
+
+    st.session_state['chain'].initialize(**st.session_state['prompts'])
+    
+    st.session_state['chain_loaded'] = True
+    # st.markdown(st.session_state['chain_loaded'])
+   
 def llm_selector():
-    if 'llm' not in st.session_state.keys():
-        st.session_state['llm'] = []
-        st.session_state['prompts'] = []
-    else:
-        st.session_state['llm'] = GLOBAL_LLM_MODELS[st.session_state['model_choice']]
-        st.session_state['llm_description'] = LLM_DESCRIPTIONS[st.session_state['model_choice']]
-        st.session_state['llm_disclaimer'] = LLM_DISCLAIMERS[st.session_state['model_choice']]
-        st.session_state['prompts'] = CHAIN_PROMPTS[st.session_state['model_choice']]
-
-        st.markdown('Note: If you want to change the LLM mid-conversation, please click "Process PDFs!" again to load the proper LLM prompts')
+    st.session_state['llm'] = GLOBAL_LLM_MODELS[st.session_state['model_choice']]
+    st.session_state['llm_description'] = LLM_DESCRIPTIONS[st.session_state['model_choice']]
+    st.session_state['llm_disclaimer'] = LLM_DISCLAIMERS[st.session_state['model_choice']]
+    st.session_state['prompts'] = CHAIN_PROMPTS[st.session_state['model_choice']]
+    # st.session_state['chain_loaded'] = False
+    print( st.session_state['model_choice'],   st.session_state['llm'], st.session_state['llm_disclaimer'])
+    st.session_state['llm_selected'] = True
+    st.session_state.messages.append({"role": "assistant", 
+                                      "content": f'Notice: You selected {st.session_state["model_choice"]} as an LLM'}
+                                      )
     
 
-# def initialize_session_states():
-    
-    # st.session_state['vector_db'] = None
-#     # st.session_state['pdfs'] = []
-#     # st.session_state['chain'] = None
 
-def main():
-    
-    # if 'llm' not in st.session_state.keys():
-    #     st.session_state['llm'] = []
-    # if 'prompts' not in st.session_state.keys():
-    #     st.session_state['prompts'] = {}
+def initialize_states():
+    if 'chain_loaded' not in st.session_state.keys():
+        st.session_state['chain_loaded'] = False
+    if 'llm_selected' not in st.session_state.keys():
+        st.session_state['llm_selected'] = False
+    if 'model_choice' not in st.session_state.keys():
+        st.session_state['model_choice'] = 'OpenAI (gpt-3.5-turbo)'
     if 'messages' not in st.session_state.keys():
         st.session_state['messages'] = []
         st.session_state.messages.append({"role": "assistant", "content": 'Hi there! If you wanna chat, please Upload a PDF on the left pane (Otherwise I might give an error!)'})
+    if 'llm' not in st.session_state.keys():
+        st.session_state['llm'] = GLOBAL_LLM_MODELS[ st.session_state['model_choice']]
+    if 'llm_description' not in st.session_state.keys():
+        st.session_state['llm_description'] = LLM_DESCRIPTIONS[ st.session_state['model_choice']]
+    if 'llm_disclaimer' not in st.session_state.keys():
+        st.session_state['llm_disclaimer'] = LLM_DISCLAIMERS[st.session_state['model_choice']]
+    if 'prompts' not in st.session_state.keys():
+        st.session_state['prompts'] = CHAIN_PROMPTS[ st.session_state['model_choice']]
+
+
+
+def main():
+
+    # if 'chain_loaded' not in st.session_state.keys():
+    #     st.session_state['chain_loaded'] = False
+    # if 'llm_selected' not in st.session_state.keys():
+    #     st.session_state['llm_selected'] = False
+    # if 'model_choice' not in st.session_state.keys():
+    #     st.session_state['model_choice'] = 'OpenAI (gpt-3.5-turbo)'
+    # if 'messages' not in st.session_state.keys():
+    #     st.session_state['messages'] = []
+    #     st.session_state.messages.append({"role": "assistant", "content": 'Hi there! If you wanna chat, please Upload a PDF on the left pane (Otherwise I might give an error!)'})
     
     st.session_state['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
     st.session_state['OPENAI_ORGANIZATION'] = st.secrets['OPENAI_ORGANIZATION']
     st.session_state['HUGGINGFACEHUB_API_TOKEN'] = st.secrets['HUGGINGFACEHUB_API_TOKEN']
 
-    
     with st.sidebar:
         st.markdown('# Instructions:')
         st.markdown('## 1. Choose an LLM model to Chat with')
         st.selectbox('Select the backend Large Language Model to use:', 
                                 ('OpenAI (gpt-3.5-turbo)', 'OpenAI (gpt-4)', 'Mistral-7B-Instruct-v0.2'), 
-                                 on_change=llm_selector, key='model_choice')
-
+                                 on_change=llm_selector, key='model_choice', 
+                                 )
+        st.markdown('Note: If you want to change the LLM mid-conversation, please click "Process PDFs!" again to load the proper LLM prompts')
         st.markdown('## 2. Upload PDF/s')
 
+        # print(st.session_state['pdfs'])
         st.file_uploader(
             label='Upload Document/s', 
             type='pdf', 
             accept_multiple_files=True, 
-            key='pdfs', 
+            key='pdfs',
+            disabled=not st.session_state['llm_selected']
             
         )
 
         st.markdown('## 3. Click Process PDFs')
-        if st.button('Process PDFs!'):
-            load_files()
-
-        
-
-            if (st.session_state['chain'] is not None) and (st.session_state['llm'] is not None):
-                st.markdown('Processing Done! LLM Fully Loaded!')
-
-        if (st.session_state['vector_db'] is not None) and (st.session_state['llm'] is not None):
-
-            st.session_state['chain'] = LCELBaseChatbot(llm=st.session_state['llm'], 
-                                            vectordb=st.session_state['vector_db'], 
-                                            message_history=st.session_state['messages'])
-
-            st.session_state['chain'].initialize(**st.session_state['prompts'])
+        st.button('Process PDFs!', on_click=load_files_and_get_chain, key='process_button')
             
-        st.markdown('## 4. Read More Information about the models')
-        st.markdown(f'### Description\n{st.session_state["llm_description"]}')
-        st.markdown(f'### Disclaimer\n{st.session_state["llm_disclaimer"]}')
+        if st.session_state['chain_loaded']:
+            st.markdown('Processing Done! LLM Fully Loaded!')
+            st.markdown('## 4. Read More Information about the models')
 
+            st.markdown(f'### Description\n{st.session_state["llm_description"]}')
 
+            st.markdown(f'### Disclaimer\n{st.session_state["llm_disclaimer"]}')
 
     show_chat_messages()
     
@@ -184,4 +195,11 @@ def main():
     
 
 if __name__ == '__main__':
+    initialize_states()
+    with st.container():
+        for k, v in st.session_state.items():
+            if k=='prompts':
+                pass
+            else:
+                st.markdown(f"{k}: {v}")
     main()
